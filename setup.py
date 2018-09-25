e6b022508675da4d2c603c10930a825813168a51
Stephen Gallagher <sgallagh@redhat.com>
2018-09-25 13:37:19 -0400

    Bump version to 0.4dev
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ModulemdTranslationHelpers',
-    version='0.3',
+    version='0.4',
     packages=['ModulemdTranslationHelpers'],
     url='https://github.com/fedora-modularity/ModulemdTranslationHelpers',
     license='MIT',
