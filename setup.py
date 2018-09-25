abaa8253c05ec4ded664e8dfb4a707642ed15e7a
Stephen Gallagher <sgallagh@redhat.com>
2018-09-25 13:11:15 -0400

    Bump version to 0.2dev
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ModulemdTranslationHelpers',
-    version='0.1',
+    version='0.2',
     packages=['ModulemdTranslationHelpers'],
     url='https://github.com/sgallagher/ModulemdTranslationHelpers',
     license='MIT',
